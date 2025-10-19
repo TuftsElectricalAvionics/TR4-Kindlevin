ae94758a4568cd93c3e68f8d5646caf7b03484b5
May <MayflowerC10@gmail.com>
2025-10-19 13:36:17 -0400

    WIP: add high-g accel (need to switch to new I2C functions)

---
+++ flight-computer/esp-idf-port/main/i2c/high_g_accel.h
@@ -0,0 +1,32 @@
+#pragma once
+#include "I2C.h"
+
+namespace seds {
+    using namespace seds::errors;
+
+    struct HighGAccelData {
+        float h_ax;
+        float h_ay;
+        float h_az;
+    };
+
+    /// ADXL375BCCZ high-g accelerometer
+    class HighGAccel {
+    public:
+        static constexpr int16_t default_address = 0x53;
+
+        /// Should this function initialize reading?
+        [[nodiscard]]
+        static Result<HighGAccel> create(I2CDevice&& device);
+
+        bool is_connected();
+
+        [[nodiscard]]
+        Result<HighGAccelData> read_acceleration();
+
+    private:
+        explicit HighGAccel(I2CDevice&& device);
+
+        I2CDevice device;
+    };
+}
