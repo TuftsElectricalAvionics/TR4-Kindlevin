2bdd563e14998f0b165ceeb655fea981b9a0b90d
May <MayflowerC10@gmail.com>
2025-10-20 12:18:50 -0400

    Add little endian read/write functions

--- flight-computer/esp-idf-port/main/utils.h
+++ flight-computer/esp-idf-port/main/utils.h
@@ -27,4 +27,28 @@ namespace seds::num {
 
         return std::bit_cast<T>(buffer);
     }
+
+    template<typename T>
+    std::array<uint8_t, sizeof(T)> to_le_bytes(T const number) {
+        static_assert(std::is_arithmetic_v<T>);
+
+        auto buffer = std::bit_cast<std::array<uint8_t, sizeof(T)>>(number);
+
+        if constexpr (std::endian::native != std::endian::little) {
+            std::ranges::reverse(buffer);
+        }
+
+        return buffer;
+    }
+
+    template<typename T>
+    T from_le_bytes(std::array<uint8_t, sizeof(T)> buffer) {
+        static_assert(std::is_arithmetic_v<T>);
+
+        if constexpr (std::endian::native != std::endian::little) {
+            std::ranges::reverse(buffer);
+        }
+
+        return std::bit_cast<T>(buffer);
+    }
 }
