c0438f304f7437f0d8b0026e5ce08a01636794fc
May <MayflowerC10@gmail.com>
2026-02-15 02:40:15 -0500

    add helpful comments

--- flight-computer/esp-idf-port/main/computer/computer.cpp
+++ flight-computer/esp-idf-port/main/computer/computer.cpp
@@ -1,5 +1,6 @@
 #include "computer.h"
 
+#include <algorithm>
 #include <cstdio>
 
 namespace seds {
@@ -20,6 +21,7 @@ void FlightComputer::process() {
         IMUData imu_data = unwrap(std::move(imu_data_try));
         // buffer is 6 floats, each max 20 char (overestimate with %s), plus 5 commas, newline, and zero terminator
         char buffer[6 * 20 + 5 + 1 + 1];
+        std::fill(std::begin(buffer), std::begin(buffer) + sizeof(buffer), 'X');
         size_t idx = 0;
         idx += snprintf(buffer, 20, "%g", imu_data.ax);
         buffer[idx] = ','; // write over previous null terminator
@@ -37,8 +39,10 @@ void FlightComputer::process() {
         buffer[idx + 2] = '\0';
 
         // if this takes too long we should rework the api to hand out file descriptors
+        // no need to flush file since this function calls fclose
+        // if we switch to file descriptors we need to explicitly flush
         sd.append_file(FlightComputer::filename, (uint8_t *)buffer, idx + 1); // add one to include newline but not null terminator
-        // TODO: FLUSH FILE!
+        // TODO ADD TIMESTAMP AND OTHER DATA
     }
 }
 
